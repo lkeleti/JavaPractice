a6ed55ee307ac00590e2b3aac5f772150900c0af
lkeleti <keleti.kaszlo@vipmail.hu>
2023-02-06 07:53:12 +0100

    change auth type at login

--- SpringSecurityExamples/Jwt-symmetric-key/frontend/auth-demo/src/stores/auth.ts
+++ SpringSecurityExamples/Jwt-symmetric-key/frontend/auth-demo/src/stores/auth.ts
@@ -15,9 +15,10 @@ export const useAuthStore = defineStore({
             const response = await fetch('http://localhost:8080/api/auth/token', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic ' + btoa(username + ":" + password)
                 },
-                body: JSON.stringify({ username, password })
+                //body: JSON.stringify({ username, password })
             });
 
             if (response.status === 200) {
@@ -26,6 +27,7 @@ export const useAuthStore = defineStore({
                 localStorage.setItem('token', JSON.stringify(token));
                 this.user = username;
                 this.token = token;
+                console.log(token);
                 router.push(this.returnUrl || '/');
             } else {
                 throw new Error('Invalid credentials');
