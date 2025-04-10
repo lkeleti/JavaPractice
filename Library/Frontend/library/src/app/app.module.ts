20e074535d7be9c2063b60eeacbeca3fef8eaf57
lkeleti <keleti.laszlo@vipmail.hu>
2025-04-10 14:02:19 +0200

    Initial Angular project setup for library frontend

---
+++ Library/Frontend/library/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
