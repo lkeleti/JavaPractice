84b312aeee1864b5f1e9e7a03e54bf8a20706079
lkeleti <keleti.kaszlo@vipmail.hu>
2025-04-18 18:53:28 +0200

    Init project

---
+++ InvotraxApp/backend/InvotraxApp/src/main/java/dev/lkeleti/invotraxapp/InvotraxAppApplication.java
@@ -0,0 +1,13 @@
+package dev.lkeleti.invotraxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvotraxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvotraxAppApplication.class, args);
+	}
+
+}
---
+++ InvotraxApp/backend/InvotraxApp/src/test/java/dev/lkeleti/invotraxapp/InvotraxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lkeleti.invotraxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvotraxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
