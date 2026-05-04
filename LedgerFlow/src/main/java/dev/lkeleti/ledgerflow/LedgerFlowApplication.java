3342bf17cc448d73fa29169d0d57ef46cb3f719f
lkeleti <keleti.kaszlo@vipmail.hu>
2026-05-04 08:24:47 +0200

    Init project

---
+++ LedgerFlow/src/main/java/dev/lkeleti/ledgerflow/LedgerFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.lkeleti.ledgerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerFlowApplication.class, args);
+	}
+
+}
